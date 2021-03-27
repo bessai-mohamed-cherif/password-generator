2b78b7c2d54c45ed856aa2f93f4d28d2e127acb8
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-03-27 16:38:48 +0100

    third commit

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,"generator/Home.html",{"password":"SRYh546Gfldj"})
+    return render(request,"generator/Home.html")
+
+def password(request):
+    return render(request,"generator/password.html")
